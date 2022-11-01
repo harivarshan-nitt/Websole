076f623dcaccb10b96d8d1a089ff6a8b18eafef2
harivarshan-nitt <harivarshan.nittrichy@gmail.com>
2022-11-01 17:25:51 +0530

    feat: test demo mysql connection

---
+++ socket/src/main/java/com/websole/socket/BlogRespository.java
@@ -0,0 +1,13 @@
+package com.websole.socket;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface BlogRespository extends JpaRepository<Blog, Integer> {
+
+    // custom query to search to blog post by title or content
+    List<Blog> findByTitleContainingOrContentContaining(String text, String textAgain);
+}
